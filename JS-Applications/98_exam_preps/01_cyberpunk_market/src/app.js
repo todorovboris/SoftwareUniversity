4a8a078332185b59380c46a7ad6d3599656f3b3a
Boris Todorov <btodorovtodorov@gmail.com>
2024-11-22 17:40:24 +0200

    add page routing for create and edit views

--- JS-Applications/98_exam_preps/01_cyberpunk_market/src/app.js
+++ JS-Applications/98_exam_preps/01_cyberpunk_market/src/app.js
@@ -1,5 +1,7 @@
 import page from '../lib/page.js';
+import createView from '../views/create.js';
 import dashboardView from '../views/dashboard.js';
+import editView from '../views/edit.js';
 
 import homeView from '../views/home.js';
 import loginView from '../views/login.js';
@@ -11,5 +13,7 @@ page('/login', loginView);
 page('/register', registerView);
 page('/logout', logoutView);
 page('/dashboard', dashboardView);
+page('/create', createView);
+page('/edit', editView);
 
 page();
