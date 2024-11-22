c1517598b1f106532bfd4da11872ec4508a53821
Boris Todorov <btodorovtodorov@gmail.com>
2024-11-22 17:33:27 +0200

    add page routing for all views in app js

--- JS-Applications/98_exam_preps/01_cyberpunk_market/src/app.js
+++ JS-Applications/98_exam_preps/01_cyberpunk_market/src/app.js
@@ -1,7 +1,15 @@
 import page from '../lib/page.js';
+import dashboardView from '../views/dashboard.js';
 
 import homeView from '../views/home.js';
+import loginView from '../views/login.js';
+import logoutView from '../views/logout.js';
+import registerView from '../views/register.js';
 
 page('/', homeView);
+page('/login', loginView);
+page('/register', registerView);
+page('/logout', logoutView);
+page('/dashboard', dashboardView);
 
 page();
