6f045f772bc5be2170e7f3d8f85c0b039b145227
Boris Todorov <btodorovtodorov@gmail.com>
2024-11-23 19:20:49 +0200

    add login function to auth file

--- JS-Applications/98_exam_preps/01_cyberpunk_market/api/auth.js
+++ JS-Applications/98_exam_preps/01_cyberpunk_market/api/auth.js
@@ -1,7 +1,7 @@
-const baseUrl = 'http://localhost:3030';
+const baseUrl = 'http://localhost:3030/users';
 
 export async function register(email, password) {
-    const response = await fetch(`${baseUrl}/users/register`, {
+    const response = await fetch(`${baseUrl}/register`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
@@ -11,6 +11,19 @@ export async function register(email, password) {
     });
 
     const userData = await response.json();
+    return userData;
+}
 
+export async function login(email, password) {
+    const response = await fetch(`${baseUrl}/login`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            email,
+            password,
+        }),
+    });
+
+    const userData = await response.json();
     return userData;
 }
--- JS-Applications/98_exam_preps/01_cyberpunk_market/views/login.js
+++ JS-Applications/98_exam_preps/01_cyberpunk_market/views/login.js
@@ -1,7 +1,28 @@
 import { html, render } from '../lib/lit-html.js';
 
-const template = () => html``;
+const template = () => html` <section id="login">
+    <div class="form">
+        <h2>Login</h2>
+        <form class="login-form">
+            <input type="text" name="email" id="email" placeholder="email" />
+            <input type="password" name="password" id="password" placeholder="password" />
+            <button type="submit">login</button>
+            <p class="message">Not registered? <a href="/register">Create an account</a></p>
+        </form>
+    </div>
+</section>`;
 
 export default async function loginView(ctx) {
     render(template());
+}
+
+async function loginFormSubmit(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    // const userData = await ÷
 }
