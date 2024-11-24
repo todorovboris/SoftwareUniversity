8c756602178986b15b9ea463834937e0c37a19a1
Boris Todorov <btodorovtodorov@gmail.com>
2024-11-24 17:19:09 +0200

    update home view

--- JS-Applications/98_exam_preps/01_cyberpunk_market/views/home.js
+++ JS-Applications/98_exam_preps/01_cyberpunk_market/views/home.js
@@ -1,6 +1,10 @@
 import { html, render } from '../lib/lit-html.js';
 
-const template = () => html` <h1>IT WORKS</h1> `;
+const template = () => html`<!-- Home page -->
+    <section id="hero">
+        <img src="./images/home.png" alt="home" />
+        <p>We know who you are, we will contact you</p>
+    </section>`;
 
 export default async function homeView(ctx) {
     render(template());
