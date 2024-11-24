eb6e867ee6d87a2c3d66b8453baa1f2e3e86487a
Boris Todorov <btodorovtodorov@gmail.com>
2024-11-24 15:45:41 +0200

    update functionality of logout to work only for login user

--- JS-Applications/98_exam_preps/01_cyberpunk_market/views/logout.js
+++ JS-Applications/98_exam_preps/01_cyberpunk_market/views/logout.js
@@ -1,11 +1,15 @@
 import { logout } from '../api/auth.js';
-import { html, render } from '../lib/lit-html.js';
 import page from '../lib/page.js';
 
 export default async function logoutView(ctx) {
     const token = localStorage.getItem('accessToken');
 
-    logout(token);
-
-    page.redirect('/');
+    if (token) {
+        try {
+            logout(token);
+            page.redirect('/');
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
 }
