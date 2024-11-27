f007f8d55cf338442d809c9d7b9642b7f0a79246
Boris Todorov <btodorovtodorov@gmail.com>
2024-11-27 23:00:32 +0200

    update home view

--- JS-Applications/98_exam_preps/02_renewTech_cleanup/views/home.js
+++ JS-Applications/98_exam_preps/02_renewTech_cleanup/views/home.js
@@ -1,6 +1,14 @@
 import { render, html } from '../lib/lit-html.js';
 
-const template = () => html`<h1>HELLO!</h1>`;
+const template = () => html` <!-- Home page -->
+    <section id="home">
+        <h1>
+            Dive into our world of cutting-edge technologies and out-of-the-box ideas designed to make Mother Nature smile again. From quirky gadgets to serious
+            solutions, we're here to show you that saving the planet can be as exciting as it is essential. Join us in our mission to turn "green" into more
+            than just a color - it's a lifestyle!
+        </h1>
+        <img id="home-img" src="./images/logo.png" alt="home-img" />
+    </section>`;
 
 export default async function homeView(ctx) {
     render(template());
