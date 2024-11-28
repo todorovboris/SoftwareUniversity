834a07f7f314fdb98e810c842ff2b5db6802c2d4
Boris Todorov <btodorovtodorov@gmail.com>
2024-11-28 21:51:34 +0200

    details view update

--- JS-Applications/98_exam_preps/02_renewTech_cleanup/app.js
+++ JS-Applications/98_exam_preps/02_renewTech_cleanup/app.js
@@ -8,6 +8,8 @@ import dashboardView from './views/dashboard.js';
 import createView from './views/create.js';
 import detailsView from './views/details.js';
 import renderNavigation from './views/navigation.js';
+import editView from './views/edit.js';
+import deleteView from './views/delete.js';
 
 page(renderNavigation);
 
@@ -17,6 +19,8 @@ page('/register', registerView);
 page('/logout', logoutView);
 page('/dashboard', dashboardView);
 page('/create', createView);
-page('/dashboard/:solutionid/details', detailsView);
+page('/dashboard/:solutionId/details', detailsView);
+page('/dashboard/:solutionId/edit', editView);
+page('/dashboard/:solutionId/delete', deleteView);
 
 page();
--- JS-Applications/98_exam_preps/02_renewTech_cleanup/views/details.js
+++ JS-Applications/98_exam_preps/02_renewTech_cleanup/views/details.js
@@ -1,7 +1,39 @@
+import { getOne } from '../api/solutions.js';
 import { render, html } from '../lib/lit-html.js';
 
-const template = () => html``;
+const template = (solution, isCreator, userId) => html`<!-- Details page -->
+    <section id="details">
+        <div id="details-wrapper">
+            <img id="details-img" src="${solution.imageUrl}" alt="example1" />
+            <div>
+                <p id="details-type">${solution.type}</p>
+                <div id="info-wrapper">
+                    <div id="details-description">
+                        <p id="description">${solution.description}</p>
+                        <p id="more-info">${solution.learnMore}</p>
+                    </div>
+                </div>
+                <h3>Like Solution:<span id="like">0</span></h3>
+                ${userId
+                    ? html`<!--Edit and Delete are only for creator-->
+                          <div id="action-buttons">
+                              ${isCreator
+                                  ? html` <a href="#" id="edit-btn">Edit</a>
+                                        <a href="#" id="delete-btn">Delete</a>`
+                                  : html` <!--Bonus - Only for logged-in users ( not authors )-->
+                                        <a href="#" id="like-btn">Like</a>`}
+                          </div>`
+                    : ''}
+            </div>
+        </div>
+    </section>`;
 
 export default async function detailsView(ctx) {
-    render(template());
+    const solutionId = ctx.params.solutionId;
+    const solution = await getOne(solutionId);
+
+    const userId = localStorage.getItem('_id');
+    const isCreator = userId === solution._ownerId;
+
+    render(template(solution, isCreator, userId));
 }
