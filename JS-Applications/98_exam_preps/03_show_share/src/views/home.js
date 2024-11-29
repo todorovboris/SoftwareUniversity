4d4bd5c17720eb34f5156299a1eecf9c695ff648
Boris Todorov <btodorovtodorov@gmail.com>
2024-11-29 17:43:15 +0200

    update home view

--- JS-Applications/98_exam_preps/03_show_share/src/views/home.js
+++ JS-Applications/98_exam_preps/03_show_share/src/views/home.js
@@ -1,6 +1,13 @@
 import { html, render } from '../../node_modules/lit-html/lit-html.js';
 
-const tempalte = () => html`<h1>HELLO!</h1>`;
+const tempalte = () => html` <!-- Home page -->
+    <section id="home">
+        <img id="home-img" src="./images/show_logo.png" alt="home-img" />
+        <h1>
+            Welcome to ShowShare, the ultimate platform for TV enthusiasts! Discover, discuss, and share your favorite TV shows with a community that loves
+            great content just as much as you do. Find hidden gems. Your next binge-worthy series is just a click away!
+        </h1>
+    </section>`;
 
 export default function homeView() {
     render(tempalte(), document.querySelector('body main'));
