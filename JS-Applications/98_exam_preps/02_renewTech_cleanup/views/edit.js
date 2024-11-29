790490940613579b820dd802fe7e405fbdc6cb3f
Boris Todorov <btodorovtodorov@gmail.com>
2024-11-29 13:26:37 +0200

    update create and edit views to take the correct fields name of the form

--- JS-Applications/98_exam_preps/02_renewTech_cleanup/views/edit.js
+++ JS-Applications/98_exam_preps/02_renewTech_cleanup/views/edit.js
@@ -9,9 +9,9 @@ const template = (solution, onSubmit) => html`<!-- Edit Page (Only for logged-in
             <h2>Edit Solution</h2>
             <form @submit=${onSubmit} class="edit-form">
                 <input type="text" value=${solution.type} name="type" id="type" placeholder="Solution Type" />
-                <input type="text" value=${solution.imageUrl} name="imageUrl" id="imageUrl" placeholder="Image URL" />
+                <input type="text" value=${solution.imageUrl} name="image-url" id="imageUrl" placeholder="Image URL" />
                 <textarea id="description" name="description" placeholder="Description" rows="2" cols="10">${solution.description}</textarea>
-                <textarea id="learnMore" name="learnMore" placeholder="more Info" rows="2" cols="10">${solution.learnMore}</textarea>
+                <textarea id="learnMore" name="more-info" placeholder="more Info" rows="2" cols="10">${solution.learnMore}</textarea>
                 <button type="submit">Edit</button>
             </form>
         </div>
@@ -27,8 +27,15 @@ export default async function editView(ctx) {
 async function editFormSubmitHandler(e) {
     e.preventDefault();
 
-    const formData = new FormData(e.currentTarget);
-    const solutionData = Object.fromEntries(formData);
+    // const formData = new FormData(e.currentTarget);
+    // const solutionData = Object.fromEntries(formData);
+    const solutionData = {
+        type: document.getElementById('type').value,
+        imageUrl: document.getElementById('imageUrl').value,
+        description: document.getElementById('description').value,
+        learnMore: document.getElementById('learnMore').value,
+    };
+
     const solutionId = this.params.solutionId;
     console.log(solutionId);
 
