99f0fa2ff3b20148b957dc47a4ea95e784a0f320
Boris Todorov <btodorovtodorov@gmail.com>
2024-11-30 16:05:59 +0200

    update navigation and home views functionality

--- JS-Applications/98_exam_preps/04_tattoo_masters/src/app.js
+++ JS-Applications/98_exam_preps/04_tattoo_masters/src/app.js
@@ -8,7 +8,10 @@ import editView from './views/edit.js';
 import homeView from './views/home.js';
 import loginView from './views/login.js';
 import logoutView from './views/logout.js';
+import navigationView from './views/navigation.js';
 import registerView from './views/register.js';
+
+page(navigationView);
 
 page('/', homeView);
 page('/login', loginView);
--- JS-Applications/98_exam_preps/04_tattoo_masters/src/views/home.js
+++ JS-Applications/98_exam_preps/04_tattoo_masters/src/views/home.js
@@ -1,7 +1,20 @@
 import { html, render } from '../../node_modules/lit-html/lit-html.js';
 
-const template = () => html`<h1>HELLO!</h1>`;
+const template = (isAuthenticated) => html`<!-- Home page -->
+    <section id="home">
+        <div id="home-wrapper">
+            <p id="home-intro">
+                Whether you're a seasoned artist, a collector of ink, or someone looking for inspiration for their first tattoo,
+                <span>Tattoo Masters</span> is your community. Share your masterpieces, discover incredible designs, and connect with artists and aficionados
+                from around the world.
+            </p>
+            <a href="/register" id="join-us">Join Us!</a>
+        </div>
+    </section>`;
 
 export default async function homeView(ctx) {
-    render(template(), document.querySelector('#content main'));
+    const accessToken = localStorage.getItem('accessToken');
+    const isAuthenticated = !accessToken;
+
+    render(template(isAuthenticated), document.querySelector('#content main'));
 }
