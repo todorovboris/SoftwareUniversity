4c62cee608cc8ae244f19a42872aa18e8db2f714
Boris Todorov <btodorovtodorov@gmail.com>
2024-11-30 14:14:55 +0200

    add all the views with their base skeleton and page them in app js file

---
+++ JS-Applications/98_exam_preps/04_tattoo_masters/src/views/login.js
@@ -0,0 +1,7 @@
+import { html, render } from '../../node_modules/lit-html/lit-html.js';
+
+const template = () => html``;
+
+export default async function loginView(ctx) {
+    render(template(), document.querySelector('#content main'));
+}
