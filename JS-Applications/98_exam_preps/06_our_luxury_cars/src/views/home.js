83f13d9bec7d6d24fbb389f07e80412ff2017725
Boris Todorov <btodorovtodorov@gmail.com>
2024-12-03 15:28:51 +0200

    add base views and page them in app js file

---
+++ JS-Applications/98_exam_preps/06_our_luxury_cars/src/views/home.js
@@ -0,0 +1,7 @@
+import { html, render } from '../../node_modules/lit-html/lit-html.js';
+
+const template = () => html`<h1>HELLO!</h1>`;
+
+export default async function homeView(ctx) {
+    render(template(), document.querySelector('#wrapper main'));
+}
