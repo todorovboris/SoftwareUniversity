ff4ccd4dc42a8fcfac3d5016e2d26a72452463a8
Boris Todorov <btodorovtodorov@gmail.com>
2024-12-03 16:29:50 +0200

    update login and logout functionality

--- JS-Applications/98_exam_preps/06_our_luxury_cars/src/api/auth.js
+++ JS-Applications/98_exam_preps/06_our_luxury_cars/src/api/auth.js
@@ -10,3 +10,22 @@ export async function register(email, password) {
     const userData = await response.json();
     return userData;
 }
+
+export async function login(email, password) {
+    const response = await fetch(`${baseUrl}/login`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password }),
+    });
+
+    const userData = await response.json();
+    return userData;
+}
+
+export async function logout(accessToken) {
+    return fetch(`${baseUrl}/logout`, {
+        headers: { 'X-Authorization': accessToken },
+    }).then(() => {
+        localStorage.clear();
+    });
+}
--- JS-Applications/98_exam_preps/06_our_luxury_cars/src/views/logout.js
+++ JS-Applications/98_exam_preps/06_our_luxury_cars/src/views/logout.js
@@ -1,7 +1,15 @@
 import { html, render } from '../../node_modules/lit-html/lit-html.js';
-
-const template = () => html``;
+import page from '../../node_modules/page/page.mjs';
+import { logout } from '../api/auth.js';
 
 export default async function logoutView(ctx) {
-    render(template(), document.querySelector('#wrapper main'));
+    const accessToken = localStorage.getItem('accessToken');
+
+    try {
+        //
+        await logout(accessToken);
+        page.redirect('/');
+    } catch (err) {
+        return alert(err.message);
+    }
 }
