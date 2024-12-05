b48fd10e182efb8ed96143fdddf67b4c52af7284
Boris Todorov <btodorovtodorov@gmail.com>
2024-12-05 14:38:42 +0200

    update navigation middleware

--- JS-Applications/98_exam_preps/08_samurider/src/views/navigation.js
+++ JS-Applications/98_exam_preps/08_samurider/src/views/navigation.js
@@ -1,30 +1,33 @@
 import { html, render } from '../../node_modules/lit-html/lit-html.js';
 import page from '../../node_modules/page/page.mjs';
 
-const template = () => html` <!-- Navigation -->
+const template = (isLoggedIn) => html` <!-- Navigation -->
     <a id="logo" href="/"><img id="logo-img" src="./images/logo.png" alt="" /></a>
 
     <nav>
         <div>
-            <a href="#">Motorcycles</a>
-            <a href="#">Search</a>
+            <a href="/dashboard">Motorcycles</a>
+            <a href="/search">Search</a>
         </div>
 
-        <!-- Logged-in users -->
-        <div class="user">
-            <a href="#">Add Motorcycle</a>
-            <a href="#">Logout</a>
-        </div>
-
-        <!-- Guest users -->
-        <div class="guest">
-            <a href="#">Login</a>
-            <a href="#">Register</a>
-        </div>
+        ${isLoggedIn
+            ? html` <!-- Logged-in users -->
+                  <div class="user">
+                      <a href="/create">Add Motorcycle</a>
+                      <a href="/logout">Logout</a>
+                  </div>`
+            : html` <!-- Guest users -->
+                  <div class="guest">
+                      <a href="/login">Login</a>
+                      <a href="/register">Register</a>
+                  </div>`}
     </nav>`;
 
 export default async function navigationView(ctx, next) {
-    render(template(), document.querySelector('#wrapper header'));
+    const accessToken = localStorage.getItem('accessToken');
+    const isLoggedIn = !!accessToken;
+
+    render(template(isLoggedIn), document.querySelector('#wrapper header'));
 
     next();
 }
