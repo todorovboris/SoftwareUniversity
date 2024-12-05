20dd93a7ed9dd7f50473089bb60726396cd59be1
Boris Todorov <btodorovtodorov@gmail.com>
2024-12-05 14:35:14 +0200

    update home view

---
+++ JS-Applications/98_exam_preps/08_samurider/src/app.js
@@ -0,0 +1,26 @@
+import page from '../node_modules/page/page.mjs';
+
+import dashboardView from './views/dashbaord.js';
+import createView from './views/create.js';
+import deleteView from './views/delete.js';
+import detailsView from './views/details.js';
+import editView from './views/edit.js';
+import homeView from './views/home.js';
+import loginView from './views/login.js';
+import logoutView from './views/logout.js';
+import registerView from './views/register.js';
+import navigationView from './views/navigation.js';
+
+page(navigationView);
+
+page('/', homeView);
+page('/login', loginView);
+page('/logout', logoutView);
+page('/register', registerView);
+page('/dashboard', dashboardView);
+page('/create', createView);
+page('/dashbaord/:itemId/details', detailsView);
+page('/dashbaord/:itemId/edit', editView);
+page('/dashbaord/:itemId/delete', deleteView);
+
+page();
--- JS-Applications/98_exam_preps/08_samurider/src/views/navigation.js
+++ JS-Applications/98_exam_preps/08_samurider/src/views/navigation.js
@@ -1,7 +1,27 @@
 import { html, render } from '../../node_modules/lit-html/lit-html.js';
 import page from '../../node_modules/page/page.mjs';
 
-const template = () => html``;
+const template = () => html` <!-- Navigation -->
+    <a id="logo" href="/"><img id="logo-img" src="./images/logo.png" alt="" /></a>
+
+    <nav>
+        <div>
+            <a href="#">Motorcycles</a>
+            <a href="#">Search</a>
+        </div>
+
+        <!-- Logged-in users -->
+        <div class="user">
+            <a href="#">Add Motorcycle</a>
+            <a href="#">Logout</a>
+        </div>
+
+        <!-- Guest users -->
+        <div class="guest">
+            <a href="#">Login</a>
+            <a href="#">Register</a>
+        </div>
+    </nav>`;
 
 export default async function navigationView(ctx, next) {
     render(template(), document.querySelector('#wrapper header'));
