0a9729c9607e7ba99327712f4978347df1ce1530
Boris Todorov <btodorovtodorov@gmail.com>
2024-12-05 14:29:03 +0200

    create the basic views

---
+++ JS-Applications/98_exam_preps/08_samurider/src/views/dashbaord.js
@@ -0,0 +1,8 @@
+import { html, render } from '../../node_modules/lit-html/lit-html.js';
+import page from '../../node_modules/page/page.mjs';
+
+const template = () => html``;
+
+export default async function dashboardView(ctx) {
+    render(template(), document.querySelector('#wrapper main'));
+}
---
+++ JS-Applications/98_exam_preps/08_samurider/src/views/navigation.js
@@ -0,0 +1,10 @@
+import { html, render } from '../../node_modules/lit-html/lit-html.js';
+import page from '../../node_modules/page/page.mjs';
+
+const template = () => html``;
+
+export default async function navigationView(ctx, next) {
+    render(template(), document.querySelector('#wrapper header'));
+
+    next();
+}
